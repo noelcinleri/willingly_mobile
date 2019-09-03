416d7ccc77dfd6fac2c53682b666247b1f07790d
Abdussamet Inanç <Abdusin@users.noreply.github.com>
2019-09-03 15:41:55 +0300

    postgres connection

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -8,14 +8,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var connection = new PostgreSQLConnection("localhost", 5432, "dart_test", username: "dart", password: "ok");
+  var connection = new PostgreSQLConnection("212.125.24.97", 5432, "Willingly", username: "WillinglyUser", password: "ok");
+  //pgsql:host=212.125.24.97;port=5432;dbname=Willingly;user=WillinglyUser;password=ok
   @override
   void initState() {
     connection.open();
     super.initState();
   }
+  Future<Widget> query()async{
+    var results = await connection.query("SELECT a, b FROM table WHERE a = @aValue", substitutionValues: {
+    "aValue" : 3
+    });
+    var a;
+    var b;
+    for (final row in results) {
+      a = row[0];
+      b = row[1];
+    } 
+    
+    return Text('');
+  }
   @override
   Widget build(BuildContext context) {
+    
     return Container(
     );
   }
