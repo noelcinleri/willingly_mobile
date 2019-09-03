2e14b8eb76ba040082e12ce866030feb632bd8c3
Umut Hope Yıldırım <umut475@gmail.com>
2019-09-03 16:19:18 +0300

    First Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
+        '/firstLogin':(context) => FirstLogin(),
       },
     );
   }
